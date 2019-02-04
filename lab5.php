86c4658fe1fbb67dee1295d2f1093d9bf41b0ae3
CMarmot <39600374+CMarmot@users.noreply.github.com>
2019-02-04 10:40:07 -0800

    Add files via upload
    I couldn't upload directly from VS because I can't get git config to work

--- lab5.php
+++ lab5.php
@@ -54,89 +54,71 @@
     <div class="container-fluid mt-3">
         <?php
             // $_GET ['aralia']
+            $aralia = false;
             if (isset($_GET['aralia'])) {
                 if($_GET['aralia'] =='yes'){
-
-        ?>
-        <h3 class="text-white p-3 shadow">Aralia</h3>
-        <div class="row mb-3">
-            <div class="col-sm-12 col-md-6 col-lg-4">
-                <div class="card mt-3 mb-3 shadow">
-                    <div class="card-header align-leff">
-                        <a href="https://www.google.com/maps/search/?api=1&query=45.63472,-122.6500">Castor Aralia
-                        </a>
-                    </div>
-                    <img class="card-img-top" src="img/trees/castor_aralia.jpg" alt="Kalopanax pictus">
-                    <div class="card-body">
-                        <p class="card-text"><strong>Common Genus:</strong> Aralia<br>
-                            <strong>Botanical Name:</strong> <em>Kalopanax pictus</em></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <?php
+                    $aralia = true;
+                }else{
+                    if($_GET['aralia'] =='no'){
+                        $aralia = false;
+                }
             }
         }
+        if(!isset($_GET['aralia'])){
+            $aralia = true;
+        }
+
+        if ($aralia == true) {
+            include 'inc/aralia.html';
+
+        }
         ?>
+        <?php
+            // $_GET ['arborvitae']
+            $arborvitae = false;
+            if (isset($_GET['arborvitae'])) {
+                if($_GET['arborvitae'] =='yes'){
+                    $arborvitae = true;
+                }else{
+                    if($_GET['arborvitae'] =='no'){
+                        $arborvitae = false;
+                }
+            }
+        }
+        if(!isset($_GET['arborvitae'])){
+            $arborvitae = true;
+        }
 
-        <h3 class="p-3 shadow">Arborvitae</h3>
-        <div class="row mb-3">
-            <div class="col-sm-12 col-md-6 col-lg-4">
-                <div class="card mt-3 mb-3 shadow">
-                    <div class="card-header shadow">
-                        <a href="https://www.google.com/maps/search/?api=1&query=45.63472,-122.65247">American
-                            Arborvitae</a>
-                    </div>
-                    <img class="card-img-top" src="img/trees/evergreen48.jpg" alt="occidentalis">
-                    <div class="card-body">
-                        <p class="card-text"><strong>Common Genus:</strong> Arborvitae<br>
-                            <strong>Botanical Name:</strong> <em>occidentalis</em></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        if ($arborvitae == true) {
+            include 'inc/arborvitae.html';
 
-        <h3 class="p-3 shadow">Ash</h3>
-        <div class="row mb-3">
-            <div class="col-sm-12 col-md-6 col-lg-4">
-                <div class="card mt-3 mb-3 shadow">
-                    <div class="card-header shadow">
-                        <a href="https://www.google.com/maps/search/?api=1&query=45.635857,-122.652392">Rose Hill Ash</a>
-                    </div>
-                    <img class="card-img-top" src="img/trees/deciduous33.jpg" alt="americana">
-                    <div class="card-body">
-                        <p class="card-text"><strong>Common Genus:</strong> Ash<br>
-                            <strong>Botanical Name:</strong> <em>americana</em></p>
-                    </div>
-                </div>
-            </div>
+        }
+        ?>
+   <?php
+            // $_GET ['ash']
+            $ash = false;
+            if (isset($_GET['ash'])) {
+                if($_GET['ash'] =='yes'){
+                    $ash = true;
+                }else{
+                    if($_GET['ash'] =='no'){
+                        $ash = false;
+                }
+            }
+        }
+        if(!isset($_GET['ash'])){
+            $ash = true;
+        }
 
-            <div class="col-sm-12 col-md-6 col-lg-4">
-                <div class="card mt-3 mb-3 shadow">
-                    <div class="card-header shadow">
-                        <a href="https://www.google.com/maps/search/?api=1&query=45.63591,-122.65031">Raywood Ash</a>
-                    </div>
-                    <img class="card-img-top" src="img/trees/deciduous34.jpg" alt="angustifolia">
-                    <div class="card-body">
-                        <p class="card-text"><strong>Common Genus:</strong> Ash<br>
-                            <strong>Botanical Name:</strong> <em>angustifolia</em></p>
-                    </div>
-                </div>
-            </div>
+        if ($ash == true) {
+            include 'inc/ash.html';
 
-            <div class="col-sm-12 col-md-6 col-lg-4">
-                <div class="card mt-3 mb-3 shadow">
-                    <div class="card-header shadow">
-                        <a href="https://www.google.com/maps/search/?api=1&query=45.635541,-122.649899">Oregon Ash</a>
-                    </div>
-                    <img class="card-img-top" src="img/trees/deciduous35.jpg" alt="latifolia">
-                    <div class="card-body">
-                        <p class="card-text"><strong>Common Genus:</strong> Ash<br>
-                            <strong>Botanical Name:</strong> ∏<em>latifolia</em></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        }
+        ?>
+      
+
+
+   
 </div>
 
 <!-- jQuery -->
